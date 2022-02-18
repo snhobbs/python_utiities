71a2fa5050d4b84c873aca8118bb3eba9f667c98
simon <simon.hobbs@electrooptical.net>
2022-02-17 20:18:36 -0500

    increasing use cases of key values pairs

--- python_utilities/data_formats.py
+++ python_utilities/data_formats.py
@@ -1,6 +1,8 @@
-# Breakup lines into key value pairs
-# Any value not in a key value pair and has none white space characters is added to a list
 def ReadKeyValueDataFile(string, header_char='#', delim = ':'):
+    '''
+    Breakup lines into key value pairs
+    Any value not in a key value pair and has none white space characters is added to a list
+    '''
     lines = string.split("\n")
     key_value = list()
     data = list()
@@ -9,9 +11,9 @@ def ReadKeyValueDataFile(string, header_char='#', delim = ':'):
         if len(line) == 0:
             continue # skip line
         if line[0] == header_char:
-            split = line.split(delim)
-            key = split[0].strip().lstrip(header_char)
-            value = delim.join(split[1:])
+            split = line.strip().split(delim)
+            key = split[0].lstrip(header_char).strip()
+            value = delim.join([pt.strip() for pt in split[1:]])
             key_value.append((key, value))
         else:
             data.append(line)
