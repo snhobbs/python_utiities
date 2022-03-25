24e7fe85e455b4b965421942ad4af9c1f50da88b
simon <simon.hobbs@electrooptical.net>
2022-03-25 17:35:36 -0400

    adding figure numbering

--- python_utilities/ipython.py
+++ python_utilities/ipython.py
@@ -1,3 +1,5 @@
+import IPython
+
 def jupyter_setup():
     from IPython.display import set_matplotlib_formats
     import matplotlib
@@ -22,3 +24,20 @@ def print_sympy(*args):
             except:
                 raise
     IPython.display.display(IPython.display.Math("".join(st)))
+
+class Figure:
+    def __init__(self):
+        self._references = dict()
+        self._count = 1
+
+    def add_reference(self, name):
+        if name not in self._references:
+            self._references[name] = self._count
+            self._count += 1
+
+    def get_reference(self, name):
+        return self._references[name]
+
+    def display(self, name : str, image : IPython.display.Image):
+        self.add_reference(name)
+        IPython.display.display(image)
